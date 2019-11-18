ce87a5e5d1b231a9fd1910192e83ade2c4b2a28a
ufoxcode <ufoxcode@gmail.com>
2019-11-18 22:30:07 +0800

    @IB Action 是從 storyborad 抓資料進來 如butoon 按鈕，textfield 字， @ib outlet 是程式把資料傳出去， 控制用。。。把 button 連了 action 和 oulet ，OUtlet才有 . is hidden

---
+++ Question Practice show answer only/QuestionStruct.swift
@@ -0,0 +1,19 @@
+//
+//  QuestionStruct.swift
+//  Question Practice show answer only
+//
+//  Created by UFO Xcode on 2019/11/18.
+//  Copyright © 2019 UFO Xcode. All rights reserved.
+//
+
+import Foundation
+
+
+struct QuestionFormat {
+    var question : String
+    var answer : String
+}
+
+
+
+
--- Question Practice show answer only/ViewController.swift
+++ Question Practice show answer only/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    
+    @IBOutlet weak var LabelQuestion: UILabel!
+    
+    @IBOutlet weak var LabelAnswer: UILabel!
+    
+    @IBOutlet weak var Abutton: UIButton!
+    
+    var questionFormat = [QuestionFormat(question: "How Old?", answer: "8"),QuestionFormat(question: "Name", answer: "Andrew"), QuestionFormat(question: "How Tall?", answer: "140 CM"), QuestionFormat(question: "How Heavy?", answer: "40 kg")]
+    
+    var index = 0
+        
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        LabelQuestion.text = questionFormat[index].question
+        
+        
     }
 
-
+    @IBAction func showAnswerButton(_ sender: UIButton) {
+        
+        LabelAnswer.text = questionFormat[index].answer
+        
+        Abutton.isHidden = true
+    }
+   
+    
+    
+          
+    @IBAction func Next(_ sender: UIButton) {
+        index += 1
+        if index == questionFormat.count{
+            index = 0
+        }
+        
+        LabelAnswer.text = ""
+        Abutton.isHidden = false
+        LabelQuestion.text = questionFormat[index].question
+        
+    }
+    
 }
 
