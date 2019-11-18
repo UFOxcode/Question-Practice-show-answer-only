b28f764a96ecc031444ba153fdedc6b8bc25c577
ufoxcode <ufoxcode@gmail.com>
2019-11-18 16:44:55 +0800

    Initial Commit

---
+++ Question Practice show answer only/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Question Practice show answer only
+//
+//  Created by UFO Xcode on 2019/11/18.
+//  Copyright © 2019 UFO Xcode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Question Practice show answer onlyTests/Question_Practice_show_answer_onlyTests.swift
@@ -0,0 +1,34 @@
+//
+//  Question_Practice_show_answer_onlyTests.swift
+//  Question Practice show answer onlyTests
+//
+//  Created by UFO Xcode on 2019/11/18.
+//  Copyright © 2019 UFO Xcode. All rights reserved.
+//
+
+import XCTest
+@testable import Question_Practice_show_answer_only
+
+class Question_Practice_show_answer_onlyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
